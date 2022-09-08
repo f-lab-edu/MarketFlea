19aa44ef86a091f14f9743e5ff136a428eb350ea
Park, Sojeong <creerari01@gmail.com>
2022-09-08 14:27:04 +0900

    init: initialize project setting

---
+++ src/test/java/com/flab/marketflea/MarketfleaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.marketflea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketfleaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
